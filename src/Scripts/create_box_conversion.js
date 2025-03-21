a4a004770e4896eb2eff9002ac1eacfaa76b4f3b
Patricia-1996 <patricia.escobar@edu.pucrs.br>
2025-03-21 17:08:10 -0300

    tentei fazer o reset, mas não deu

--- src/Scripts/create_box_conversion.js
+++ src/Scripts/create_box_conversion.js
@@ -1,6 +1,6 @@
 let stage = localStorage.getItem('stage')
 
-function convert() {
+function convert() {O problema com a sua função 
     stage = localStorage.getItem('stage')
     if (stage == null) {
         document.body.classList.add("stage_two")
@@ -8,10 +8,29 @@ function convert() {
     }
 }
 
-function reset() {
-    stage = localStorage.getItem('stage')
-    if (stage !== null) {
-        document.body.classList.remove("stage_two")
-        localStorage.setItem('stage', null)
+document.addEventListener('DOMContentLoaded',function reset() {
+    
+    localStorage.removeItem('stage');
+    document.body.classList.remove("stage_two");
+
+    const resultDiv = document.getElementById('result');
+    const currentRateDiv = document.getElementById('cambioatual');
+    const containerGrafico = document.getElementById("container-grafico");
+
+    if(resultDiv) resultDiv.textContent = '';
+    if(currentRateDiv) currentRateDiv.textContent = '';
+    if(containerGrafico) containerGrafico.innerHTML = '';
+
+    updateFlag('from-currency', 'from-curr-flag-img');
+    updateFlag('to-currency', 'to-curr-flag-img');
+    document.addEventListener('DOMContentLoaded', function())
+    const amountInput = document.getElementById('amount');
+    amountInput.value = '';
+    
+    const resetBtn = document.getElementById('reset-btn');
+    if(resetBtn){
+        resetBtn.addEventListener('click', reset);
     }
+    });
+
 }
