be618f222a4f7bbb3147dbed7b68711e4eedbb6d
Aline Ibaldo Gonçalves <aline_ibaldo@hotmail.com>
2025-03-25 03:15:40 -0300

    códigos sem alteração

---
+++ src/Scripts/create_box_conversion.js
@@ -0,0 +1,17 @@
+let stage = localStorage.getItem('stage')
+
+function convert() {
+    stage = localStorage.getItem('stage')
+    if (stage == null) {
+        document.body.classList.add("stage_two")
+        localStorage.setItem('stage','stage-two')
+    }
+}
+
+function reset() {
+    stage = localStorage.getItem('stage')
+    if (stage !== null) {
+        document.body.classList.remove("stage_two")
+        localStorage.setItem('stage', null)
+    }
+}
